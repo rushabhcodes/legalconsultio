1906deb33bad634cd256f20b16873510121c2da6
Rushabh1134 <97875843+Rushabh1134@users.noreply.github.com>
2023-04-29 02:00:21 +0530

    Update 404.tsx

--- pages/404.tsx
+++ pages/404.tsx
@@ -8,7 +8,7 @@ const NotFound = () => {
 
       <div className="flex justify-center p-10 border-solid shadow-2xl mx-40">
         <h1 className="text-3xl pb-10 px-10 justify-center text-center">
-          Uh oh, it looks like you've hit a dead end. Good thing our lawyers
+          Uh oh, it looks like you have hit a dead end. Good thing our lawyers
           are experts at finding loopholes.
         </h1>
       </div>
