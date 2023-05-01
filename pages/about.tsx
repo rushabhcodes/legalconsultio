9d3d6728f4721d83d95ee12e2ddb7c609c3e1444
Rushabh1134 <97875843+Rushabh1134@users.noreply.github.com>
2023-05-01 19:13:37 +0530

    GraphQL

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,134 +1,97 @@
-import Footer from "@/components/Footer";
+import { GetStaticProps } from "next";
+import Image from "next/image";
+import { request, gql } from "graphql-request";
 import Navbar from "@/components/Navbar";
-import React from "react";
+import Footer from "@/components/Footer";
 
-const about = () => {
+interface Post {
+  name: string;
+  dp: {
+    url: string;
+    fileName: string;
+  };
+}
+
+interface PostsData {
+  posts: Post[];
+}
+
+export const getStaticProps: GetStaticProps<{ data: PostsData }> = async () => {
+  const query = gql`
+    query Posts {
+      posts {
+        name
+        dp {
+          url
+          fileName
+        }
+      }
+      
+    }
+  `;
+
+  const data = await request<PostsData>(
+    "https://ap-south-1.cdn.hygraph.com/content/clh39efxt4w3q01um86kee6dq/master",
+    query
+  );
+
+  return {
+    props: {
+      data,
+    },
+    revalidate: 60 * 60, // 1 hour
+  };
+};
+
+function AboutPage({ data }: { data: PostsData }) {
+  const posts = data.posts;
+
   return (
     <>
-      <div className="bg-about bg-fixed bg-repeat-round  h-auto lg:h-80">
-        <Navbar />
+      <Navbar />
+      <div className="max-w-screen bg-about bg-fixed bg-repeat-round h-80">
         <h1 className="font-bold flex justify-center text-center align-middle pt-40 text-6xl">
           About Us
         </h1>
       </div>
-      <div className>Our Founders</div>
-<div className="grid grid-cols-2">
-  <div>
-       {/* Desigination of founder */}
-       <div className="flex justify-center">
-        <h1 className="font-bold text-4xl mt-5">Founder</h1>
+      <div className="flex justify-center pt-10">
+        <h1 className="font-bold text-center text-6xl p-10 pb-10">
+          The Founders
+        </h1>
       </div>
-      
-      {/* Image  of founder */}
-      <div className="flex justify-center">
-        <img
-          className="rounded-full h-96 w-96"
-          src="https://thumbs.dreamstime.com/b/flat-male-avatar-image-beard-hairstyle-businessman-profile-icon-vector-179285629.jpg"
-          alt="Founder Image"
-        />
+      {posts.map((post) => (
+        <div key={post.name}>
+          <div className="grid justify-items-center justify-center justify- grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 gap-10 px-10 py-10 ">
+            <div className="flex justify-center p-10 shadow-2xl rounded-full">
+              <Image
+                className="rounded-full"
+                src={post.dp.url}
+                alt={post.dp.fileName}
+                width={300}
+                height={300}
+              />
+            </div>
+            <div className="py-10">
+              <div className="font-bold text-3xl mb-2">{post.name}</div>
+              <p className="text-gray-700 text-base pr-12">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Voluptatibus quia, nulla! Maiores et perferendis eaque,
+                exercitationem praesentium nihil.
+              </p>
+            </div>
+          </div>
+        </div>
+      ))}
+
+      <div className="flex justify-center pt-10">
+        <h1 className="font-bold text-center text-6xl p-10 pb-10">
+          Our Team
+        </h1>
       </div>
 
-      {/* Name of founder */}
-      <div className="flex justify-center">
-        <h1 className="font-semibold text-3xl">Mr. John Doe</h1>
-      </div>
-      </div>
-
-<div>
-      {/* Desigination of co-founder */}
-      <div className="flex justify-center">
-        <h1 className="font-bold text-4xl mt-10">Co-Founder</h1>
-      </div>
-
-      {/* Image of co-founder */}
-      <div className="flex justify-center">
-        <img
-          className="rounded-full h-96 w-96"
-          src="https://static.vecteezy.com/system/resources/previews/006/735/770/original/beautiful-woman-avatar-profile-icon-vector.jpg"
-          alt="Co-Founder Image"
-        />
-      </div>
-
-      {/* Name of co-founder */}
-      <div className="flex justify-center">
-        <h1 className="font-semibold text-3xl">Mr. Jane Doe</h1>
-      </div>
-      </div>
-      </div>
-
-<div className="">Our Team</div>
-      <div className="grid grid-cols-2">
-  <div>
-       {/* Desigination of founder */}
-       <div className="flex justify-center">
-        <h1 className="font-bold text-4xl mt-5">Founder</h1>
-      </div>
-      
-      {/* Image  of founder */}
-      <div className="flex justify-center">
-        <img
-          className="rounded-full h-96 w-96"
-          src="https://thumbs.dreamstime.com/b/flat-male-avatar-image-beard-hairstyle-businessman-profile-icon-vector-179285629.jpg"
-          alt="Founder Image"
-        />
-      </div>
-
-      {/* Name of founder */}
-      <div className="flex justify-center">
-        <h1 className="font-semibold text-3xl">Mr. John Doe</h1>
-      </div>
-      </div>
-
-<div>
-      {/* Desigination of co-founder */}
-      <div className="flex justify-center">
-        <h1 className="font-bold text-4xl mt-10">Co-Founder</h1>
-      </div>
-
-      {/* Image of co-founder */}
-      <div className="flex justify-center">
-        <img
-          className="rounded-full h-96 w-96"
-          src="https://static.vecteezy.com/system/resources/previews/006/735/770/original/beautiful-woman-avatar-profile-icon-vector.jpg"
-          alt="Co-Founder Image"
-        />
-      </div>
-
-      {/* Name of co-founder */}
-      <div className="flex justify-center">
-        <h1 className="font-semibold text-3xl">Mr. Jane Doe</h1>
-      </div>
-      </div>
-      </div>
-      
-
-      {/* description */}
-      <div className="flex justify-center">
-        <p className="text-center font-semibold text-2xl p-5 mt-5  px-5 lg:px-40">
-          We are a team of lawyers who are here to help you with your legal
-          queries. We have a team of lawyers who are experts in their fields and
-          are here to help you with your legal issues. We are here to help you
-          with your legal issues and queries.
-        </p>
-      </div>
-
-      <div>
-        <p className="text-center font-semibold text-xl p-5 mt-5 px-5 lg:px-40">
-          While you are facing any sort of a legal questioning, you must not be
-          aware from where to start your journey from. Indeed it is a daunting
-          task to evaluate the legal issues along with choosing the perfect
-          lawyer to handle your legal things as per your requirements. Searching
-          for a lawyer is an easy task but searching for the RIGHT LAWYER is
-          what we can serve you right here to give you the One Stop Solution
-          with our team of experts. We here at Dahanu Lawyers are right here to
-          assist you with your personal and your financial stakes. Be it
-          corporate or the general clients, since it is not that easy to gather
-          the facts we help you out with every bit of it.
-        </p>
-      </div>
       <Footer />
     </>
   );
-};
+}
 
-export default about;
+export default AboutPage;
