1c4a57b4575cec6770d61f4f8b651df8f0a15fd4
Rushabh1134 <97875843+Rushabh1134@users.noreply.github.com>
2023-05-01 22:19:23 +0530

    Updated: Banking Partners bg

--- components/BankingPartners.tsx
+++ components/BankingPartners.tsx
@@ -4,9 +4,9 @@ import Link from 'next/link'
 
 const BankingPartners = () => {
   return (
-    <>
-    <div><h1 className='text-center font-semibold text-3xl p-10  text-white bg-gray-600'>Our Banking Partners</h1></div>
-    <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 gap-20 p-20 text-white bg-gray-500 px-40 '>
+    <><hr/>
+    <div><h1 className='text-center font-semibold text-3xl p-10 bg-slate-200'>Our Banking Partners</h1></div>
+    <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 gap-20 p-20 bg-white px-40 '>
         <Link href={"https://www.kotak.com/en/home.html"}><motion.div whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.8 }} className='shadow-lg shadow-black p-5 rounded-md'><h3 className='text-center'></h3>
         <img
                 className=" overflow-auto"
--- pages/about.tsx
+++ pages/about.tsx
@@ -4,29 +4,58 @@ import { request, gql } from "graphql-request";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 
-interface Post {
+interface Person {
   name: string;
   dp: {
     url: string;
     fileName: string;
   };
+  content: {
+    html: string;
+  };
 }
 
 interface PostsData {
-  posts: Post[];
+  founders: Person[];
+  employees: Person[];
+  interns: Person[];
 }
 
 export const getStaticProps: GetStaticProps<{ data: PostsData }> = async () => {
   const query = gql`
     query Posts {
-      posts {
+      founders {
         name
         dp {
           url
           fileName
         }
+        content {
+          html
+        }
       }
-      
+
+      employees {
+        name
+        dp {
+          url
+          fileName
+        }
+        content {
+          html
+        }
+      }
+
+      interns {
+        name
+        dp {
+          url
+          fileName
+        }
+        content {
+          html
+        }
+      }
     }
   `;
 
@@ -44,7 +73,7 @@ export const getStaticProps: GetStaticProps<{ data: PostsData }> = async () => {
 };
 
 function AboutPage({ data }: { data: PostsData }) {
-  const posts = data.posts;
+  const { founders, employees, interns } = data;
 
   return (
     <>
@@ -59,20 +88,49 @@ function AboutPage({ data }: { data: PostsData }) {
           The Founders
         </h1>
       </div>
-      {posts.map((post) => (
-        <div key={post.name}>
+
+      {founders.map((person) => (
+        <div key={person.name}>
           <div className="grid justify-items-center justify-center justify- grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 gap-10 px-10 py-10 ">
             <div className="flex justify-center p-10 shadow-2xl rounded-full">
               <Image
                 className="rounded-full"
-                src={post.dp.url}
-                alt={post.dp.fileName}
+                src={person.dp.url}
+                alt={person.dp.fileName}
                 width={300}
                 height={300}
               />
             </div>
             <div className="py-10">
-              <div className="font-bold text-3xl mb-2">{post.name}</div>
+              <div className="font-bold text-3xl mb-2">{person.name}</div>
+              <p className="text-gray-700 text-base pr-12">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Voluptatibus quia, nulla! Maiores et perferendis eaque,
+                exercitationem praesentium nihil.
+              </p>
+            </div>
+          </div>
+        </div>
+      ))}
+
+      <div className="flex justify-center pt-10">
+        <h1 className="font-bold text-center text-6xl p-10 pb-10">Our Team</h1>
+      </div>
+
+      {employees.map((person) => (
+        <div key={person.name}>
+          <div className="grid justify-items-center justify-center justify- grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 gap-10 px-10 py-10 ">
+            <div className="flex justify-center p-10 shadow-2xl rounded-full">
+              <Image
+                className="rounded-full"
+                src={person.dp.url}
+                alt={person.dp.fileName}
+                width={300}
+                height={300}
+              />
+            </div>
+            <div className="py-10">
+              <div className="font-bold text-3xl mb-2">{person.name}</div>
               <p className="text-gray-700 text-base pr-12">
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit.
                 Voluptatibus quia, nulla! Maiores et perferendis eaque,
@@ -85,13 +143,129 @@ function AboutPage({ data }: { data: PostsData }) {
 
       <div className="flex justify-center pt-10">
         <h1 className="font-bold text-center text-6xl p-10 pb-10">
-          Our Team
+          Our Interns
         </h1>
       </div>
-
+      {interns.map((person) => (
+        <div key={person.name}>
+          <div className="grid justify-items-center justify-center justify- grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 gap-10 px-10 py-10 ">
+            <div className="flex justify-center p-10 shadow-2xl rounded-full">
+              <Image
+                className="rounded-full"
+                src={person.dp.url}
+                alt={person.dp.fileName}
+                width={300}
+                height={300}
+              />
+            </div>
+            <div className="py-10">
+              <div className="font-bold text-3xl mb-2">{person.name}</div>
+              <p className="text-gray-700 text-base pr-12">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Voluptatibus quia, nulla! Maiores et perferendis eaque,
+                exercitationem praesentium nihil.
+              </p>
+            </div>
+          </div>
+        </div>
+      ))}
       <Footer />
     </>
   );
 }
 
 export default AboutPage;
+
+// import { GetStaticProps } from "next";
+// import Image from "next/image";
+// import { request, gql } from "graphql-request";
+// import Navbar from "@/components/Navbar";
+// import Footer from "@/components/Footer";
+
+// interface Post {
+//   name: string;
+//   dp: {
+//     url: string;
+//     fileName: string;
+//   };
+// }
+
+// interface PostsData {
+//   posts: Post[];
+// }
+
+// export const getStaticProps: GetStaticProps<{ data: PostsData }> = async () => {
+//   const query = gql`
+//     query Posts {
+//       posts {
+//         name
+//         dp {
+//           url
+//           fileName
+//         }
+//       }
+
+//     }
+//   `;
+
+//   const data = await request<PostsData>(
+//     "https://ap-south-1.cdn.hygraph.com/content/clh39efxt4w3q01um86kee6dq/master",
+//     query
+//   );
+
+//   return {
+//     props: {
+//       data,
+//     },
+//     revalidate: 60 * 60, // 1 hour
+//   };
+// };
+
+// function AboutPage({ data }: { data: PostsData }) {
+//   const posts = data.posts;
+
+//   return (
+//     <>
+//       <Navbar />
+//       <div className="max-w-screen bg-about bg-fixed bg-repeat-round h-80">
+//         <h1 className="font-bold flex justify-center text-center align-middle pt-40 text-6xl">
+//           About Us
+//         </h1>
+//       </div>
+//
+// {posts.map((post) => (
+//   <div key={post.name}>
+//     <div className="grid justify-items-center justify-center justify- grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 gap-10 px-10 py-10 ">
+//       <div className="flex justify-center p-10 shadow-2xl rounded-full">
+//         <Image
+//           className="rounded-full"
+//           src={post.dp.url}
+//           alt={post.dp.fileName}
+//           width={300}
+//           height={300}
+//         />
+//       </div>
+//       <div className="py-10">
+//         <div className="font-bold text-3xl mb-2">{post.name}</div>
+//         <p className="text-gray-700 text-base pr-12">
+//           Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+//           Voluptatibus quia, nulla! Maiores et perferendis eaque,
+//           exercitationem praesentium nihil.
+//         </p>
+//       </div>
+//     </div>
+//   </div>
+// ))}
+
+//       <div className="flex justify-center pt-10">
+//         <h1 className="font-bold text-center text-6xl p-10 pb-10">
+//           Our Team
+//         </h1>
+//       </div>
+
+//
+//     </>
+//   );
+// }
+
+// export default AboutPage;
