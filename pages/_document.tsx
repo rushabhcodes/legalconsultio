386b449449f0bdbe8c5592ff17c1964ef7896084
Rushabh1134 <97875843+Rushabh1134@users.noreply.github.com>
2023-12-12 05:42:27 +0000

    SEO Optimization. Before Rank - 30

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,9 +7,14 @@ export default function Document() {
         <link rel="icon" href="/favicon/favicon.ico" />
         <meta
           name="description"
-          content="Legalconsultio by Prakash Thakkar - Where law meets results.
+          content="Legalconsultio by Prakash Thakkar - Best lawyer in dahanu.
 At legalconsultio we handle claims at all levels of litigation. Our areas of practice include Business Law, Employment and Labor Law, Intellectual Property/Technology Transactions, Family Law, Civil and Criminal Law.
 You can call our office or email us at legalconsultio@gmail.com with any questions in regards to your case or on any other matter. We are committed to returning all calls and emails within 24 hours.
+Want to find best lawyers in dahanu -> Prakash Thakkar i.e. Legalconsultio.
+Prakash Thakkar is here for all your legal consultation needs.
+
+
+
 "
         />
       </Head>
